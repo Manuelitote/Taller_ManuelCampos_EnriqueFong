ea8c65bb4608889a90325336ee2a448ee59c9d00
Manuelitote <moneyrex2704@gmail.com>
2025-10-10 09:45:02 -0500

    comentarios prob 9

--- Problema9.php
+++ Problema9.php
@@ -14,7 +14,7 @@ require_once 'Navegacion.php';
     <div class="container">
         <h2>Problema #9</h2>
         <p>Calcular las 15 primeras potencias del número </p>
-
+ <!-- Formulario HTML donde el usuario introduce un número -->
     <form method="POST">
         <label>Ingrese un número del 1 al 9:</label>
         <input type="number" name="numero" min="1" max="9" required placeholder="Ej: 4">
@@ -22,11 +22,14 @@ require_once 'Navegacion.php';
     </form>
 <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Convierte el valor enviado en un número entero (por seguridad)
     $numero = intval($_POST['numero']);
     
     if ($numero >= 1 && $numero <= 9) {
         $potencias = OperacionesMatematicas::obtenerPotencias($numero, 15);
         
+
+     // Imprime la tabla con los resultados
         echo '<div class="container">
                 <h2>Las 15 Primeras Potencias de ' . $numero . '</h2>
                 <table>
@@ -34,7 +37,7 @@ require_once 'Navegacion.php';
                         <th>Expresión</th>
                         <th>Resultado</th>
                     </tr>';
-        
+     // Recorre cada potencia calculada y muestra una fila por cada resultado   
         foreach ($potencias as $potencia) {
             echo '<tr>
                     <td>' . $numero . ' <sup>' . $potencia['exponente'] . '</sup></td>
@@ -42,6 +45,7 @@ require_once 'Navegacion.php';
                   </tr>';
         }
         
+     // Cierra la tabla HTML    
         echo '</table></div>';
     } else {
         echo '<div>
